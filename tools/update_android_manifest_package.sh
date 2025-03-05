1d228fadbd3d50f2192fb30e161e68a564d1c407
Amer Ghazal <amer.ghazal@droneleaf.io>
2025-03-05 17:25:41 +0400

    Final Update

--- tools/update_android_manifest_package.sh
+++ tools/update_android_manifest_package.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-MANIFEST_FILE=LeafMC/android/AndroidManifest.xml
+# check if SOURCE_DIR variable is set
+if [ -z "$SOURCE_DIR" ]; then
+    MANIFEST_FILE=android/AndroidManifest.xml
+else
+    MANIFEST_FILE=$SOURCE_DIR/android/AndroidManifest.xml
+fi
 
 echo "Adjusting package name for daily build"
 QGC_PKG_NAME="org.mavlink.qgroundcontrolbeta"
