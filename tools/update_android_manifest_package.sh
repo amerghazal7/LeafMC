81148059ca4299c76a153b65d5a7a85953592811
Amer Ghazal <amer.ghazal@droneleaf.io>
2025-03-06 14:19:43 +0400

    Update Android build related files

--- tools/update_android_manifest_package.sh
+++ tools/update_android_manifest_package.sh
@@ -2,6 +2,18 @@
 
 MANIFEST_FILE=android/AndroidManifest.xml
 
+if [ ! -f $MANIFEST_FILE ]; then
+    echo "Error: AndroidManifest.xml not found"
+    exit 1
+fi
+
+# check o=if $SOURCE_DIR is set
+if [ -z "$SOURCE_DIR" ]; then
+    MANIFEST_FILE=android/AndroidManifest.xml
+else
+    MANIFEST_FILE=$SOURCE_DIR/android/AndroidManifest.xml
+fi
+
 echo "Adjusting package name for daily build"
 QGC_PKG_NAME="org.mavlink.qgroundcontrolbeta"
 sed -i -e 's/package *= *"[^"]*"/package="'$QGC_PKG_NAME'"/' $MANIFEST_FILE
